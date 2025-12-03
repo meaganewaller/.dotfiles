3465fb1086c67e345f0cf11b8311193dcc10ba68
Meagan Waller <meagan@meaganwaller.com>
2025-12-03 16:06:50 -0500

    Adding some work related fish stuff

---
+++ home/.config/fish/conf.d/00_work.fish
@@ -0,0 +1,4 @@
+if test -f "$HOME/.gusto/init.fish"
+  set -gx GUSTO_SSH_SELF_MANAGED 1
+  set -gx DOTFILES_PROFILE work
+end
--- home/.config/fish/conf.d/10_mise.fish
+++ home/.config/fish/conf.d/10_mise.fish
@@ -1,17 +1,21 @@
-begin
+if test -f "$HOME/.gusto/init.fish"
+  source $HOME/.gusto/init.fish
+else
+  begin
     if type -q $HOME/.local/bin/mise
-        if status is-interactive
-            $HOME/.local/bin/mise activate fish | source
-        else
-            $HOME/.local/bin/mise activate fish --shims | source
-        end
+      if status is-interactive
+        $HOME/.local/bin/mise activate fish | source
+      else
+        $HOME/.local/bin/mise activate fish --shims | source
+      end
 
-        abbr mnode 'mise x -- node'
-        abbr mnpm 'mise x -- npm'
-        abbr mnvim 'mise x -- nvim'
-        abbr myarn 'mise x -- yarn'
-        abbr mpnpm 'mise x -- pnpm'
-        abbr mbun 'mise x -- bun'
-        abbr mruby 'mise x -- ruby'
+      abbr mnode 'mise x -- node'
+      abbr mnpm 'mise x -- npm'
+      abbr mnvim 'mise x -- nvim'
+      abbr myarn 'mise x -- yarn'
+      abbr mpnpm 'mise x -- pnpm'
+      abbr mbun 'mise x -- bun'
+      abbr mruby 'mise x -- ruby'
     end
+  end
 end
