d3c74ca119f1afa91f45a9d6d9116fed3481bd12
Meagan Waller <meagan@meaganwaller.com>
2026-03-05 14:14:51 -0500

    feat(paths): add trash-cli binary path to Fish configuration
    This commit enhances the Fish shell configuration by adding the path for the trash-cli binary, improving accessibility for users who utilize this tool. The new path is conditionally added if the directory exists, maintaining the existing structure for other paths.

--- home/.config/fish/conf.d/00_paths.fish
+++ home/.config/fish/conf.d/00_paths.fish
@@ -6,6 +6,10 @@ if test -d /opt/homebrew/sbin
     fish_add_path /opt/homebrew/sbin
 end
 
+if test -d /opt/homebrew/opt/trash-cli/bin
+    fish_add_path /opt/homebrew/opt/trash-cli/bin
+end
+
 if test -d "$HOME/.local/bin"
     fish_add_path --universal "$HOME/.local/bin"
 end
