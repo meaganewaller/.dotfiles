ebed15c0dfa1f02bdb911d2dee1843b466395b3a
Meagan Waller <meagan@meaganwaller.com>
2026-05-07 14:31:01 -0400

    feat(fish): enhance shell configuration and add new tools
    - Introduced new Fish configuration files for improved environment management, including support for zoxide, bat, and starship.
    - Added functions for handling git diffs and enhanced ls functionality using eza for better visual output.
    - Updated starship configuration for a more customizable prompt experience.
    - Removed obsolete files and streamlined existing configurations to enhance organization and maintainability.

---
+++ packages/shells/fish/conf.d/starship-init.fish
@@ -0,0 +1,24 @@
+# Init starship prompt
+#
+# doc: https://starship.rs
+# Quick install: `curl -fsSL https://starship.rs/install.sh | bash`
+ 
+# if not type -q starship
+#     curl -fsSL https://starship.rs/install.sh -o /tmp/starship-install.sh
+#     mkdir -p ~/.local/bin
+#     bash /tmp/starship-install.sh -b $HOME/.local/bin -y
+#     rm /tmp/starship-install.sh
+# end
+
+if status --is-interactive
+  if type -q starship
+      set -l ctx (set -q DOTPICKLES_ROLE; and echo $DOTPICKLES_ROLE; or echo personal)
+      if test -f /.dockerenv; or test -n "$REMOTE_CONTAINERS"; or test -n "$CODESPACES"
+          set -gx STARSHIP_CTX "$ctx | devcontainer"
+      else
+          set -gx STARSHIP_CTX $ctx
+      end
+
+      starship init fish | source
+  end
+end
